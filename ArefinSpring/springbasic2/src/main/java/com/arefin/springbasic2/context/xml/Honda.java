fa399707d3734cd2644fec530fb153249343d93c
Safayat_Arefin <safayatarefin@gmail.com>
2019-03-22 18:56:21 +0600

    3/22/2019Night

---
+++ ArefinSpring/springbasic2/src/main/java/com/arefin/springbasic2/context/xml/Honda.java
@@ -0,0 +1,8 @@
+package com.arefin.springbasic2.context.xml;
+
+public class Honda implements Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Drive a Honda");
+    }
+}
